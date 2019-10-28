a7a157116fa6d51e28890683157958d2853c8b09
Julio <julioqq29@gmail.com>
2019-10-28 11:21:05 -0500

    Interface search user

--- app/src/main/java/com/example/graphql/MainActivity.kt
+++ app/src/main/java/com/example/graphql/MainActivity.kt
@@ -1,12 +1,140 @@
 package com.example.graphql
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.AdapterView
+import android.widget.ListView
+import android.widget.Toast
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.example.graphql.adapter.AdapterUser
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var apolloClient: ApolloClient
+    private var adapterUser: AdapterUser? = null
+    private var listView: ListView? = null
+
+    companion object {
+        val Log = Logger.getLogger(MainActivity::class.java.name)
+        private const val BASE_URL = "https://api.github.com/graphql"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this@MainActivity, "Init!!!!!", Toast.LENGTH_SHORT).show()
+
+        apolloClient = setupApollo()
+
+        /*apolloClient.query(
+            FindUsersQuery
+                .builder()
+                .name("julit")
+                .items(20)
+                .build()
+        )
+            .enqueue(object : ApolloCall.Callback<FindUsersQuery.Data>() {
+
+                override fun onFailure(e: ApolloException) {
+                    Log.info(e.message.toString())
+                }
+
+                override fun onResponse(response: Response<FindUsersQuery.Data>) {
+                    Log.info(" " + response.data()?.search())
+
+                    runOnUiThread {
+
+
+                        listView = listSearchedUsers as ListView
+                        /*val user = response.data()?.search()?.edges()?.get(0)?.node()
+                        if (user is FindUsersQuery.AsUser) {
+                            Toast.makeText(this@MainActivity, user.name, Toast.LENGTH_SHORT).show()
+                        }*/
+                        val userQueryList = response.data()?.search()?.edges()
+                        adapterUser = AdapterUser(this@MainActivity, userQueryList!!)
+                        listView!!.adapter = adapterUser
+                        //adapterUser!!.notifyDataSetChanged()
+
+                        /*listView!!.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+                            val intent = Intent(this@MainActivity, GetAllUserRepository::class.java)
+                            intent.putExtra("user", userQueryList[position])
+                            startActivity(intent)
+                        }*/
+                    }
+                }
+            })*/
+
+        editTextUserName.setOnEditorActionListener { v, actionId, event ->
+
+            if(actionId == EditorInfo.IME_ACTION_SEARCH) {
+
+                Toast.makeText(this@MainActivity, "Pass Cond!!!!!", Toast.LENGTH_SHORT).show()
+
+                apolloClient.query(
+                    FindUsersQuery
+                        .builder()
+                        .name(editTextUserName.text.toString())
+                        .items(20)
+                        .build()
+                )
+                .enqueue(object : ApolloCall.Callback<FindUsersQuery.Data>() {
+
+                    override fun onFailure(e: ApolloException) {
+                        Log.info(e.message.toString())
+                    }
+
+                    override fun onResponse(response: Response<FindUsersQuery.Data>) {
+                        Log.info(" " + response.data()?.search())
+
+                        runOnUiThread {
+                            listView = listSearchedUsers as ListView
+                            //val user = response.data()?.search()?.edges()?.get(0)?.node()
+                            /*if (user is FindUsersQuery.AsUser) {
+                                Toast.makeText(this@MainActivity, user.name(), Toast.LENGTH_SHORT).show()
+                            }*/
+                            val userQueryList = response.data()?.search()?.edges()
+                            adapterUser = AdapterUser(this@MainActivity, userQueryList!!)
+                            listView!!.adapter = adapterUser
+                            //adapterUser!!.notifyDataSetChanged()
+
+                            /*listView!!.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+                                val intent = Intent(this@MainActivity, GetAllUserRepository::class.java)
+                                intent.putExtra("user", userQueryList[position])
+                                startActivity(intent)
+                            }*/
+                        }
+                    }
+                })
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun setupApollo(): ApolloClient {
+        val okHttp = OkHttpClient
+            .Builder()
+            .addInterceptor { chain ->
+                val original = chain.request()
+                val builder = original.newBuilder().method(original.method(), original.body())
+                builder.addHeader("Authorization"
+                    , "Bearer " + BuildConfig.AUTH_TOKEN)
+                chain.proceed(builder.build())
+            }
+            .build()
+        return ApolloClient.builder()
+            .serverUrl(BASE_URL)
+            .okHttpClient(okHttp)
+            .build()
     }
 }
---
+++ app/src/main/java/com/example/graphql/adapter/AdapterUser.kt
@@ -0,0 +1,67 @@
+package com.example.graphql.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.graphql.FindUsersQuery
+import com.example.graphql.R
+
+class AdapterUser (private val context: Context, private val userQueryList: List<FindUsersQuery.Edge>) :
+    BaseAdapter(){
+
+    override fun getCount(): Int {
+        return userQueryList.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return userQueryList[position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return 0
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var convertView = convertView
+        val holder: ViewHolder
+
+        if (convertView == null) {
+            holder = ViewHolder()
+            val inflater = context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            convertView = inflater.inflate(R.layout.user_item, null, true)
+
+            holder.itemNameLocation = convertView!!.findViewById(R.id.userNameLocation) as TextView
+            holder.itemLogin = convertView.findViewById(R.id.userLogin) as TextView
+            holder.itemNumRepos = convertView.findViewById(R.id.userNumRepos) as TextView
+            holder.itemPathAvatar = convertView.findViewById(R.id.userAvatar) as ImageView
+
+            convertView.tag = holder
+        } else {
+            holder = convertView.tag as ViewHolder
+        }
+
+        val userNode = userQueryList[position].node()
+
+        if (userNode is FindUsersQuery.AsUser) {
+            holder.itemNameLocation!!.text = userNode.name() + (if (userNode.location() != null ) ", " + userNode.location() else "")
+            holder.itemLogin!!.text = userNode.login()
+            holder.itemNumRepos!!.text = "# Repos:  " + userNode.repositories().totalCount()
+            holder.itemPathAvatar!!.setImageResource(R.drawable.github_avatar)
+        }
+
+        return convertView
+    }
+
+    private inner class ViewHolder {
+
+        var itemNameLocation: TextView? = null
+        var itemLogin: TextView? = null
+        var itemNumRepos: TextView? = null
+        var itemPathAvatar: ImageView? = null
+    }
+}
